87609a40d80609496028698cc92f56c99d875441
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2016-09-29 13:03:32 -0700

    finished practice for Exam 1

--- Extra/Exam1/problem1a.js
+++ Extra/Exam1/problem1a.js
@@ -1,9 +1,8 @@
-  var initialValue = prompt("Please enter your starting value here");
-  var finalValue = prompt("Please enter where you want the counter to stop");
-  var increment = prompt("Please enter how much you want your values to increase by");
-
+  var initialValue = parseInt(prompt("Please enter your starting value here"));
+  var finalValue = parseInt(prompt("Please enter where you want the counter to stop"));
+  var increment = parseInt(prompt("Please enter how much you want your values to increase by"));
+  $("#footer").append("<div>" + initialValue + "</div>");
   while (initialValue < finalValue) {
       initialValue = initialValue + increment;
-      alert(initialValue);
       $("#footer").append("<div>" + initialValue + "</div>");
   };
