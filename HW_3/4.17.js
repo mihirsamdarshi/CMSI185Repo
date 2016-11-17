28eba4687fb9014bc91455b982bccfc004dbe97c
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2016-11-16 20:38:08 -0800

    added python exercise 5

--- HW_3/4.17.js
+++ HW_3/4.17.js
@@ -1,22 +1,21 @@
 var userInput = prompt("Enter a number between 1 - 10,000 here")
 if (userInput == NaN) {
     alert("Please try again")
-}
-else if (userInput > 10000) {
+} else if (userInput > 10000) {
     alert("Please try again")
-}
-
-function collatzNumber() {
-    var n = userInput
-    var steps = 0
-    while (n > 1) {
-        if (n % 2 === 0) {
-            n / 2;
-            steps += 1;
-        } else if (n % 2 === 1) {
-            n * 3 + 1;
-            steps += 1;
+} else {
+    function collatzNumber() {
+        var n = userInput
+        var steps = 0
+        while (n > 1) {
+            if (n % 2 === 0) {
+                n / 2;
+                steps += 1;
+            } else if (n % 2 === 1) {
+                n * 3 + 1;
+                steps += 1;
+            }
         }
+        alert(steps)
     }
-    alert(steps)
 }
---
+++ HW_3/Exercise_5.py
@@ -0,0 +1,9 @@
+rentals = float(input("Enter the number of movie rentals: "))
+referrals = float(input("Enter the number of member referrals: "))
+
+def memberdiscount():
+    discount = rentals + referrals
+    for discount in range(0,76)
+    print("The discount is equal to " discount " percent")
+    else
+    print ("The discount is equal to 75 percent")
